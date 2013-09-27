54da4b5e6a57bef1dc4143f076106f383dd6b5c3
Dewey Vozel <dewey.vozel@gmail.com>
2013-09-27 12:43:50 -0400

    Created a new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkbook::Application.config.secret_token = 'b4c38878b28e87b925933c305404cb4b968052acda515f5e039e4c63251a9a44e15e41c6d52b32c5349de599ec58c9bf2a8ae60b6756a0b15c9d8f241a6ea256'
